96376220f4de98329f2cc984e119bd8098c9e762
kalmarek <kalmar@amu.edu.pl>
2020-05-24 23:10:09 +0200

    first version of Words

--- src/Word.jl
+++ src/Word.jl
@@ -1,68 +1,80 @@
-abstract type Word{T} <: AbstractVector{T} end
+"""
+    AbstractWord{T} <: AbstractVector{T}
+Abstract type representing words over an Alphabet.
 
+`AbstractWord` as such has its meaning only in the contex of an Alphabet.
+The subtypes of `AbstractWord{T}` need to implement the following methods which
+constitute `AbstractWord` interface:
+ * `Base.==`: the equality (as words),
+ * `Base.hash`: simple uniqueness hashing function
+ * `Base.one` the empty word (i.e. monoid identity element)
+ * `Base.push!`/`Base.pushfirst!`: appending a single value at the end/beginning
+ * `Base.append!`/`Base.prepend!`: appending a another word at the end/beginning,
+ * `Base.:*` for words concatentation (monoid binary operation)
+ * full iteration protocol for `AbstractArray`s, returning pointers to letters
+of an Alphabet
+ * `length` the length of word as written in the alphabet.
 
+Note that `length` represents how word is written and not the shortest form of
+e.g. free reduced word.
 
-#interface for morphism T->Int
-function to_int{T}(t::T) end
-#interface for morphism Int->T
-function from_int{T}(t::T) end
-#interface for ordering on T
-function <=(a::T,b::T) end
-#empty string as monoid neutral element
-Base.one(::Type{Word{T}}) where {T}  = Word(Vector{T}())
-#overload for <= that implements word ordering
-function <=(a::Word{T},b::Word{T})
-#overload for * as concatenation
-function *(a::Word{T},b::Word{T}) where {T} Word(a.ptrs+b.ptrs) end
-#interface for function that returns actual vector inside word
-function data(a::Word{T})
+Iteration over an `AbstractWord` may produce negative numbers. In such case the
+inverse (if exists) of the pointed generator is meant.
+"""
 
-Base.length(w::Word{T}) where {T}  = length(w.ptrs)
+abstract type AbstractWord{T} <: AbstractVector{T} end
 
+"""
+    Word{T} <: AbstractWord{T}
+Word as written in an alphabet storing only pointers to letters of an Alphabet.
 
-#Is there function composition in Julia?
-#Is it possible to assign function more or less like this?
-#In Haskell I could just make it work
-function Base.iterate(w::Word{T}) = data(w) . iterate()
-
-struct WordLexLen{T}<:Word{T}
+Note that the negative values in `genptrs` field represent the inverse of letter.
+"""
+struct Word{T} <: AbstractWord{T}
     ptrs::Vector{T}
 end
 
-struct WordLex{T}<:Word{T}
-    ptrs::Vector{T}
+Word(x::AbstractVector{<:Integer}) = Word{Int16}(x) # setting the default type
+
+Base.:(==)(w::Word, v::Word) = w.prtrs == v.ptrs
+Base.hash(w::Word{T}, h::UInt) =
+    foldl((h, x) -> hash(x, h), w.ptrs, init = hash(0x352c2195932ae61e, h))
+# the init value is simply hash(Word)
+
+Base.one(w::Word{T}) where T = Word{T}(T[])
+
+Base.push!(w::Word{T}, n::Integer) = push!(w.ptrs, n)
+Base.pushfirst!(w::Word{T}, n::Integer) = pushfirst!(w.ptrs, n)
+Base.append!(w::Word{T}, v::Word{T}) where T = (append!(w.ptrs, v.ptrs); w)
+Base.prepend!(w::Word{T}, v::Word{T}) where T = (preapend!(w.ptrs, v.ptrs); w)
+Base.:*(w::Word{S}, v::Word{T}) where {S,T} =
+    (TT = promote_type(S,T); Word{TT}(TT[w.ptrs; v.ptrs]))
+
+Base.iterate(w::Word) = iterate(w.ptrs)
+Base.iterate(w::Word, state) = iterate(w.ptrs, state)
+Base.size(w::Word) = size(w.ptrs)
+
+Base.similar(w::Word, ::Type{S}) where S = Word{S}(similar(w.ptrs, S))
+
+Base.@propagate_inbounds function Base.getindex(w::Word, n::Integer)
+    @boundscheck checkbounds(w, n)
+    return @inbounds w.ptrs[n]
 end
 
-#Julia does not have dependet types, but type constructors
-#can be emulated by manually specyfing all inhabitants of type family.
-#In this case the type family is {WordLexLen,WordLex}
-
-#This can be though of as one of the "axioms" for Word
-function data(a::WordLexLen{T})
-  return a.ptrs
+Base.@propagate_inbounds function Base.setindex!(w::Word, v::Integer, n::Integer)
+    @boundscheck checkbounds(w, n)
+    return @inbounds w.ptrs[n] = v
 end
 
-function data(a::WordLex{T})
-  return a.ptrs
-end
-
-#This can be though of as another "axiom" for Word
-function <=(a::WordLexLen{T},b::WordLexLen{T})
-  if length(a) < length(b) return true end
-  if length(a) > length(b) return false end
-  for (char_in_a,char_in_b) in zip(a,b)
-    if char_in_a > char_in_b
-      return false
+"""
+    inv(w::Word)
+Return the inverse of given word by reversing and negating its pointers.
+"""
+function Base.inv(w::Word)
+    res = similar(w)
+    n = length(w)
+    @inbounds for (i, l) in enumerate(w)
+        res[n-1+i] = -l
     end
-  end
-  return true
-end
-
-function <=(a::WordLex{T},b::WordLex{T})
-  for (char_in_a,char_in_b) in zip(a,b)
-    if char_in_a > char_in_b
-      return false
-    end
-  end
-  return true
+    return res
 end
