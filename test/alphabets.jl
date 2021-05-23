1d6c3f681b2670c0137aa482ef717cf877373ec2
Marek Kaluba <kalmar@amu.edu.pl>
2021-05-23 23:24:15 +0200

    update tests
    * operations on rws systems
    * alphabets

--- test/alphabets.jl
+++ test/alphabets.jl
@@ -4,47 +4,59 @@
     @test Alphabet{Char}() isa Alphabet{Char}
     @test_throws AssertionError Alphabet{Int}()
     @test_throws AssertionError Alphabet([1, 2, 3])
-
-    A = Alphabet{Char}()
-    @test length(A.letters) == 0 && length(A.inversions) == 0
-    @test sprint(show, A) isa String
-
-    B = Alphabet(['a', 'b', 'c'])
-    @test sprint(show, B) isa String
-    @test B isa Alphabet{Char}
-    @test length(B.letters) == 3 && length(B.inversions) == 3
-    @test findfirst(i -> i != 0, B.inversions) === nothing
-
     @test_throws AssertionError Alphabet(['a', 'b', 'a'])
 
-    push!(A, 'a', 'b', 'c')
-    @test length(A.letters) == 3 && length(A.inversions) == 3
-    @test findfirst(i -> i != 0, A.inversions) === nothing
-    @test_throws ErrorException push!(A, 'a')
+    let B = Alphabet(['a', 'b', 'c'])
+        @test B isa Alphabet{Char}
+        @test sprint(show, B) isa String
+        @test sprint(show, MIME"text/plain"(), B) isa String
+        @test length(B.letters) == 3 && length(B.inversions) == 3
+        @test findfirst(i -> i != 0, B.inversions) === nothing
 
-    @test A[1] == 'a' && A[2] == 'b' && A[3] == 'c'
-    @test A['a'] == 1 && A['b'] == 2 && A['c'] == 3
+        @test B[1] == 'a' && B[2] == 'b' && B[3] == 'c'
+        @test B['a'] == 1 && B['b'] == 2 && B['c'] == 3
+        @test_throws DomainError B['d']
+    end
 
-    @test B[1] == 'a' && B[2] == 'b' && B[3] == 'c'
-    @test B['a'] == 1 && B['b'] == 2 && B['c'] == 3
-    @test_throws DomainError B['d']
+    let A = Alphabet{Char}()
+        @test sprint(show, A) isa String
+        @test sprint(show, MIME"text/plain"(), A) isa String
 
-    @test_throws ErrorException set_inversion!(A, 'd', 'e')
-    @test_throws ErrorException set_inversion!(A, 'a', 'e')
+        @test length(A.letters) == 0 && length(A.inversions) == 0
 
-    set_inversion!(A, 'a', 'b')
-    @test A[-2] == 'a' && A[-1] == 'b'
-    set_inversion!(A, 'b', 'c')
-    @test A[-2] == 'c' && A[-3] == 'b'
-    set_inversion!(A, 'a', 'c')
-    @test A[-1] == 'c' && A[-3] == 'a'
-    @test_throws DomainError A[-2]
+        push!(A, 'a', 'b', 'c')
+        @test length(A.letters) == 3 && length(A.inversions) == 3
+        @test findfirst(i -> i != 0, A.inversions) === nothing
+        @test_throws ErrorException push!(A, 'a')
 
-    @test A[A[1]] == 1
+        @test A[1] == 'a' && A[2] == 'b' && A[3] == 'c'
+        @test A['a'] == 1 && A['b'] == 2 && A['c'] == 3
 
-    w = KnuthBendix.Word([1, 2])
-    @test_throws DomainError inv(A, w) # b is not invertible
-    w = KnuthBendix.Word([1, 1, 3])
-    @test inv(A, w) == Word([1, 3, 3]) # inv(a*a*c)
-    @test inv(A, inv(A, w)) == w
+        @test_throws ErrorException set_inversion!(A, 'd', 'e')
+        @test_throws ErrorException set_inversion!(A, 'a', 'e')
+
+        set_inversion!(A, 'a', 'b')
+        @test A[-2] == 'a' && A[-1] == 'b'
+        set_inversion!(A, 'b', 'c')
+        @test A[-2] == 'c' && A[-3] == 'b'
+        set_inversion!(A, 'a', 'c')
+        @test A[-1] == 'c' && A[-3] == 'a'
+        @test_throws DomainError A[-2]
+
+        @test sprint(show, A) isa String
+        @test sprint(show, MIME"text/plain"(), A) isa String
+
+        @test A[A[1]] == 1
+    end
+
+    @testset "Inverting using Alphabet" begin
+        A = Alphabet(['a', 'b', 'A'])
+        KnuthBendix.set_inversion!(A, 'a', 'A')
+
+        w = KnuthBendix.Word([1, 2])
+        @test_throws DomainError inv(A, w) # b is not invertible
+        w = KnuthBendix.Word([1, 1, 3])
+        @test inv(A, w) == Word([1, 3, 3]) # inv(a*a*c)
+        @test inv(A, inv(A, w)) == w
+    end
 end
