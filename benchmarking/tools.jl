3dda7ce6d1a27d2589891a823a77aee2e8e51af6
RearrangedLetters <RobinLink@ProtonMail.com>
2023-07-20 13:03:27 +0200

    Do minor reformatting.

--- benchmarking/tools.jl
+++ benchmarking/tools.jl
@@ -2,5 +2,7 @@ using KnuthBendix
 
 function rwsfromfile(filepath)
     filecontent = String(read(filepath))
-    return RewritingSystem(KnuthBendix.parse_kbmag(filecontent, method = :string))
+    return RewritingSystem(
+        KnuthBendix.parse_kbmag(filecontent, method = :string),
+    )
 end
