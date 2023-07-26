6c7974ff3cca08a0653c400d7b3f4979df9c1269
Marek Kaluba <kalmar@amu.edu.pl>
2023-07-26 16:30:40 +0200

    cleanup kb2idxA_parallel_1!

--- src/knuthbendix_idxA_parallel.jl
+++ src/knuthbendix_idxA_parallel.jl
@@ -7,99 +7,65 @@ function kb2idxA_parallel_1!(
 ) where {W}
     rws = reduce!(rws)
     idxA = IndexAutomaton(rws)
-    tasks_per_thread = 1  # TODO: This currently fails for values greater than 1
-    # stacks = Channel{Vector{Tuple{W,W}}}(nthreads())
-    # foreach(
-    #     _ -> put!(stacks, Vector{Tuple{W,W}}()),
-    #     1:nthreads()*tasks_per_thread,
-    # )
-    workspace_type = typeof(Workspace(rws, idxA))
-    workspaces = Channel{workspace_type}(2nthreads())
-    foreach(
-        _ -> put!(workspaces, Workspace(rws, idxA)),
-        1:2nthreads()*tasks_per_thread,
-    )
-    confluence_workspace = Workspace(rws, idxA)
+    tasks_per_thread = 1
 
-    i = firstindex(rws.rwrules)
-    lck = Threads.SpinLock()
+    confl_workspace = Workspace(rws, idxA)
+    workspace_type = typeof(confl_workspace)
+    workspaces = Channel{workspace_type}(2nthreads())
+    for _ in 1:2nthreads()*tasks_per_thread
+        w = Workspace(rws, idxA)
+        put!(workspaces, w)
+    end
 
     stack = Vector{Tuple{W,W}}()
+
+    i = firstindex(rws.rwrules)
     while i ≤ lastindex(rws.rwrules)
+        @info i
         ri = rws.rwrules[i]
 
-        confluence_workspace.confluence_timer += 1
-        if time_to_check_confluence(rws, confluence_workspace, settings)
+        confl_workspace.confluence_timer += 1
+        if time_to_check_confluence(rws, confl_workspace, settings)
             if !isempty(stack)
-                rws, idxA, i, _ = Automata.rebuild!(
-                    idxA,
-                    rws,
-                    stack,
-                    i,
-                    0,
-                    confluence_workspace,
-                )
+                rws, idxA, i, _ =
+                    Automata.rebuild!(idxA, rws, stack, i, 0, confl_workspace)
                 @assert isempty(stack)
             end
-            stack = check_confluence!(stack, rws, idxA, confluence_workspace)
+            stack = check_confluence!(stack, rws, idxA, confl_workspace)
             isempty(stack) && return rws
         end
 
-        j = firstindex(rws.rwrules)
-        num_found = 0
-
-        stack =
-            let chunk_size =
-                    max(50, i ÷ (tasks_per_thread * Threads.nthreads())),
-                data_chunks = partition(1:i, chunk_size)
-
-                intermediate_results = map(data_chunks) do index_range
-                    @spawn begin
-                        local_stack = Vector{Tuple{W,W}}()
-                        workspace = Workspace(rws, idxA)
-                        # workspace = take!(workspaces)
-                        for j in index_range
-                            rj = rws.rwrules[j]
-                            local_stack = find_critical_pairs!(
-                                local_stack,
-                                idxA,
-                                ri,
-                                rj,
-                                workspace,
-                            )
-                            if ri !== rj
-                                local_stack = find_critical_pairs!(
-                                    local_stack,
-                                    idxA,
-                                    rj,
-                                    ri,
-                                    workspace,
-                                )
-                            end
-                            # Threads.atomic_add!(num_found, length(local_stack))
-                            lock(lck) do
-                                return num_found += length(local_stack)
-                            end
+        begin
+            chunk_size = max(50, i ÷ (tasks_per_thread * Threads.nthreads()))
+            chunks = partition(1:i, chunk_size)
+            local_stacks = map(chunks) do index_range
+                @spawn begin
+                    tl_stack = Vector{Tuple{W,W}}()
+                    # workspace = Workspace(rws, idxA)
+                    workspace = take!(workspaces)
+                    for j in index_range
+                        rj = rws.rwrules[j]
+                        find_critical_pairs!(tl_stack, idxA, ri, rj, workspace)
+                        if ri !== rj
+                            find_critical_pairs!(tl_stack, idxA, rj, ri, workspace)
                         end
-                        # put!(workspaces, workspace)
-                        # put!(stacks, Vector{Tuple{W,W}}())
-                        return local_stack::Vector{Tuple{W,W}}
                     end
+                    put!(workspaces, workspace)
+                    return tl_stack::Vector{Tuple{W,W}}
                 end
-                # @info intermediate_results
-                stacks = fetch.(intermediate_results)
-                # @info length.(stacks)
-                append!(stack, stacks...)
             end
 
-        # TODO: This causes allocations that likely are avoidable by changing functions
-        # such as deriverule! or instead by defining a datastructure that allows
-        # for e.g. pop!
+            stacks = fetch.(local_stacks)
+        end
+        # @info length.(stacks)
+        append!(stack, stacks...)
 
-        if num_found[] > 0 && time_to_rebuild(rws, stack, settings)
-            rws, idxA, i, j =
-                Automata.rebuild!(idxA, rws, stack, i, j, confluence_workspace)
+        if length(stack) > 0 && time_to_rebuild(rws, stack, settings)
+            wrkspce = take!(workspaces)
+            @time rws, idxA, i, j =
+                Automata.rebuild!(idxA, rws, stack, i, 0, wrkspce)
             @assert isempty(stack)
+            put!(workspaces, wrkspce)
         end
 
         if settings.verbosity > 0
@@ -109,10 +75,11 @@ function kb2idxA_parallel_1!(
         end
 
         if i == lastindex(rws.rwrules) && !isempty(stack)
-            @debug "reached end of rwrules with $(length(stack)) rules on stack"
-            rws, idxA, i, _ =
-                Automata.rebuild!(idxA, rws, stack, i, 0, confluence_workspace)
+            @info "reached end of rwrules with $(length(stack)) rules on stack"
+            wrkspce = take!(workspaces)
+            rws, idxA, i, _ = Automata.rebuild!(idxA, rws, stack, i, 0, wrkspce)
             @assert isempty(stack)
+            put!(workspaces, wrkspce)
         end
         i += 1
     end
