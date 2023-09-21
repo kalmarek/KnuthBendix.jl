8b53f7bc2fe9ff715bcccdb6cfed857bf7a3fca1
Marek Kaluba <kalmar@amu.edu.pl>
2023-09-21 20:06:58 +0200

    bring back kbmag_settings

--- benchmarking/tools.jl
+++ benchmarking/tools.jl
@@ -1,5 +1,3 @@
-using KnuthBendix
-
 function rwsfromfile(filepath; method = :string)
     @assert isfile(filepath)
     filecontent = read(filepath, String)
@@ -20,3 +18,13 @@ function clean_kbprog(file)
         isfile(fn) && rm(fn)
     end
 end
+
+# The default parameters were obtained from
+# gap-packages.github.io/kbmag/doc/chap2_mj.html#X7BB411528630D4E9
+const kbmag_settings = KnuthBendix.Settings(
+    max_rules = 32767,    # maxeqns
+    confluence_delay = 500,      # confnum
+    max_length_rhs = 0,        # (max_rhs, _)
+    max_length_lhs = 0,        # (_, max_lhs)
+    max_length_overlap = 0,        # maxoverlaplen
+)
