b2991d02f64ebd5f27d70346e21163e76f2f3f35
Marek Kaluba <kalmar@amu.edu.pl>
2023-09-29 23:23:51 +0200

    rewrite knuthbendix based on the CompletionAlgorithm
    subtypes of CompletionAlgorithm are used for dispatch

--- src/knuthbendix_base.jl
+++ src/knuthbendix_base.jl
@@ -11,46 +11,45 @@ function are_we_stopping(rws::RewritingSystem, settings::Settings)
     return false
 end
 
-function reduce!(
-    rws::RewritingSystem,
-    work::Workspace = Workspace(rws);
-    sort_rules = true,
-)
-    remove_inactive!(rws)
-    if sort_rules
-        sort!(rws.rwrules, by = length ∘ first, rev = true)
-        # shortest rules are at the end of rwrules...
-    end
-    # ...so that they endup on the top of the stack
-    stack = [(first(r), last(r)) for r in rws.rwrules]
-    empty!(rws)
-
-    deriverule!(rws, stack, work)
-    @assert isempty(stack)
-
-    if sort_rules
-        reverse!(rws.rwrules)
-        sort!(rws.rwrules, by = length ∘ first, alg = Base.Sort.InsertionSort)
-    end
-
-    return rws
-end
-
 ## General interface
 
+abstract type CompletionAlgorithm end
+
+end
+
+"""
+    knuthbendix(rws::RewritingSystem[, settings=Settings()])
+    knuthbendix(method::CompletionAlgorithm, rws::RewritingSystem[, settings:Settings()])
+Perform Knuth-Bendix completion on rewriting system `rws` using algorithm
+defined by `method`.
+
+A reduced rewriting system is returned.
+"""
 function knuthbendix(
+    method::CompletionAlgorithm,
     rws::RewritingSystem,
-    settings::Settings = Settings();
-    implementation::Symbol = :index_automaton,
+    settings = Settings();
 )
-    R = deepcopy(rws)
+    rws_dc = deepcopy(rws)
     try
-        return knuthbendix!(R, settings; implementation = implementation)
+        prog = Progress(
+            length(rws.rwrules),
+            desc = "Knuth-Bendix completion ",
+            showspeed = true,
+            enabled = settings.verbosity > 0,
+        )
+
+        settings.update_progress = (args...) -> _kb_progress(prog, args...)
+
+        rws_dc = knuthbendix!(method, rws_dc, settings)
+        finish!(prog)
+
+        return rws_dc
     catch e
-        if e == InterruptException()
+        if e isa InterruptException
             @warn "Received user interrupt in Knuth-Bendix completion.
             Returned rws is reduced, but not confluent"
-            return reduce!(R)
+            return reduce!(method, rws_dc)
         else
             rethrow(e)
         end
@@ -81,46 +80,4 @@ function _kb_progress(prog::Progress, total, current, on_stack)
         )],
     )
     return prog
-end
-
-function knuthbendix!(
-    rws::RewritingSystem,
-    settings::Settings;
-    implementation::Symbol = :index_automaton,
-)
-    impl_list = (:naive_kbs1, :naive_kbs2, :rule_deletion, :index_automaton)
-    implementation in impl_list || throw(
-        ArgumentError(
-            "Implementation \"$implementation\" of Knuth-Bendix completion is not defined.\n Possible choices are: $(join(impl_list, ", ", " and ")).",
-        ),
-    )
-
-    if implementation == :naive_kbs1
-        if settings.verbosity > 0
-            @info "knuthbendix1 is a simplistic implementation for educational purposes only."
-        end
-        return knuthbendix1!(rws, settings)
-    end
-
-    prog = Progress(
-        count(isactive, rws.rwrules),
-        desc = "Knuth-Bendix completion ",
-        showspeed = true,
-        enabled = settings.verbosity > 0,
-    )
-
-    settings.update_progress = (args...) -> _kb_progress(prog, args...)
-
-    kb_impl! = if implementation == :naive_kbs2
-        knuthbendix2!
-    elseif implementation == :rule_deletion
-        knuthbendix2deleteinactive!
-    elseif implementation == :index_automaton
-        knuthbendix2automaton!
-    end
-
-    reduce!(rws)
-    rws = kb_impl!(rws, settings)
-    finish!(prog)
-    return rws
 end
