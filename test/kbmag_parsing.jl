a413dd812cc255834e4fc7acec0f4d8f21bff30f
Marek Kaluba <kalmar@amu.edu.pl>
2023-10-01 17:43:29 +0200

    update tests for changes to KbmagRWS

--- test/kbmag_parsing.jl
+++ test/kbmag_parsing.jl
@@ -16,7 +16,7 @@ using MacroTools
             # file_content = join(split(file_content), "")
             rws = KnuthBendix.parse_kbmag(file_content, method = :string)
 
-            @test rws.generators ==
+            @test rws.generatorOrder ==
                   [Symbol("g.10"), Symbol("g.20"), Symbol("g.30")]
             @test rws.inverses == [1, 3, 2]
             @test rws.equations == [[2, 2] => [3], [3, 1, 3] => [1, 2, 1]]
@@ -25,7 +25,7 @@ using MacroTools
         let file_content = String(read(joinpath(kb_data, "3a6")))
             # file_content = join(split(file_content), "")
             rws = KnuthBendix.parse_kbmag(file_content, method = :ast)
-            @test rws.generators == [:a, :b, :A, :B]
+            @test rws.generatorOrder == [:a, :b, :A, :B]
             @test rws.inverses == [3, 4, 1, 2]
             @test rws.equations == [
                 [1, 1, 1] => Int[],
@@ -57,8 +57,8 @@ using MacroTools
             method = (fn == "a4" ? :string : :ast)
             rws = KnuthBendix.parse_kbmag(file_content, method = method)
             @test rws isa KnuthBendix.KbmagRWS
-            @test !isempty(rws.generators)
-            @test length(rws.generators) == length(rws.inverses)
+            @test !isempty(rws.generatorOrder)
+            @test length(rws.generatorOrder) == length(rws.inverses)
             @test all(!isempty(lhs) for (lhs, rhs) in rws.equations)
             rws
         end
