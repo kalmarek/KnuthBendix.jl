3ab45559acca3562c5e54622b057aacdded57f94
Marek Kaluba <kalmar@amu.edu.pl>
2024-07-19 15:10:06 +0200

    add timings for kbmag examples to track them

--- test/kbmag_parsing.jl
+++ test/kbmag_parsing.jl
@@ -88,3 +88,62 @@ using MacroTools
         @test isconfluent(R)
     end
 end
+
+#=
+[ Info: 237
+  0.000361 seconds (1.21 k allocations: 79.055 KiB)
+[ Info: 237_8
+  1.194131 seconds (104.70 k allocations: 20.352 MiB)
+[ Info: 3a6
+  0.010471 seconds (9.26 k allocations: 775.977 KiB)
+[ Info: a4
+  0.000105 seconds (541 allocations: 34.258 KiB)
+[ Info: a4monoid
+  0.000052 seconds (330 allocations: 22.961 KiB)
+[ Info: ab1
+  0.000031 seconds (233 allocations: 15.086 KiB)
+[ Info: ab2
+  0.000064 seconds (445 allocations: 29.023 KiB)
+[ Info: c2
+  0.000028 seconds (217 allocations: 14.086 KiB)
+[ Info: cosets
+  0.000225 seconds (1.03 k allocations: 66.195 KiB)
+[ Info: d22
+  0.000581 seconds (2.15 k allocations: 157.500 KiB)
+[ Info: degen1
+  0.000025 seconds (196 allocations: 12.789 KiB)
+[ Info: degen2
+  0.000031 seconds (231 allocations: 14.398 KiB)
+[ Info: degen3
+  0.000068 seconds (319 allocations: 19.477 KiB)
+[ Info: degen4a
+  0.005365 seconds (4.65 k allocations: 392.031 KiB)
+[ Info: e8
+  0.472674 seconds (60.31 k allocations: 6.680 MiB, 1.12% gc time)
+[ Info: f2
+  0.000110 seconds (306 allocations: 19.945 KiB)
+[ Info: f25
+  0.001225 seconds (4.30 k allocations: 251.086 KiB)
+[ Info: f25monoid
+  0.000322 seconds (1.13 k allocations: 76.422 KiB)
+[ Info: freenilpc3
+  0.000904 seconds (2.69 k allocations: 220.523 KiB)
+[ Info: l32ext
+  1.436185 seconds (110.77 k allocations: 21.370 MiB)
+[ Info: m11
+  5.466974 seconds (199.91 k allocations: 43.134 MiB, 0.10% gc time)
+[ Info: nilp2
+  0.000224 seconds (856 allocations: 62.953 KiB)
+[ Info: nonhopf
+  0.000108 seconds (599 allocations: 41.500 KiB)
+[ Info: s16
+  0.015451 seconds (23.77 k allocations: 1.550 MiB)
+[ Info: s3
+  0.000058 seconds (274 allocations: 17.211 KiB)
+[ Info: s4
+  0.000101 seconds (595 allocations: 37.594 KiB)
+[ Info: s9
+  0.000888 seconds (2.65 k allocations: 176.445 KiB)
+[ Info: torus
+  0.000141 seconds (826 allocations: 61.352 KiB)
+=#
