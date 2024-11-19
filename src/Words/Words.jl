991f4dcdc527460eb8d20eefc5ec5ca2b33e255c
Marek Kaluba <kalmar@amu.edu.pl>
2024-11-19 12:21:07 +0100

    bring back specialized copyto! for words

--- src/Words/Words.jl
+++ src/Words/Words.jl
@@ -8,18 +8,18 @@ include("words_impl.jl")
 include("bufferwords.jl")
 include("searchindex.jl")
 
-# copyto! performance (??)
-# Base.copyto!(w::Word, v::Word) = (Base.copyto!(w.ptrs, v.ptrs); w)
-# function Base.copyto!(w::Word, v::BufferWord)
-#     Base.copyto!(w.ptrs, @view v.storage[v.lidx:v.ridx])
-#     return w
-# end
-# function Base.copyto!(w::BufferWord, v::Word)
-#     @assert internal_length(w) ≥ length(v)
-#     Base.copyto!(w.storage, v.ptrs)
-#     w.lidx = 1
-#     w.ridx = length(v)
-#     return w
-# end
+# make copyto! allocation free on julia-1.6
+Base.copyto!(w::Word, v::Word) = (Base.copyto!(w.ptrs, v.ptrs); w)
+function Base.copyto!(w::Word, v::BufferWord)
+    Base.copyto!(w.ptrs, @view v.storage[v.lidx:v.ridx])
+    return w
+end
+function Base.copyto!(w::BufferWord, v::Word)
+    @assert internal_length(w) ≥ length(v)
+    Base.copyto!(w.storage, v.ptrs)
+    w.lidx = 1
+    w.ridx = length(v)
+    return w
+end
 
 end
